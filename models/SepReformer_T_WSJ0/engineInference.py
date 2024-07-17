55f564f286b3ba5f1cd0ffd88a99dfeb371530e5
Mervin Qi <mervin.qi@ringcentral.com>
2024-07-17 17:44:43 +0800

    add some inference process

--- models/SepReformer_T_WSJ0/engineInference.py
+++ models/SepReformer_T_WSJ0/engineInference.py
@@ -51,6 +51,10 @@ class Engine(object):
     @logger_wraps()
     def _inference(self, mixture, frames, mxiture_file, wav_dir=None):
         self.model.eval()
+        no_win = 0
+        out_win = 0
+        out_linear = 0
+        win_in_win_out = 1
         with torch.inference_mode():
             nnet_input = torch.tensor(mixture, device=self.device)
 
@@ -60,22 +64,133 @@ class Engine(object):
                 on_test_end = time.time()
                 cost_time = on_test_end - on_test_start
                 print("Total inference non_chunk------------:", cost_time)
-
-            elif 1:
+            elif no_win:
                 estim_src_0 = torch.zeros(1, nnet_input.size(1))
                 estim_src_1 = torch.zeros(1, nnet_input.size(1))
 
                 for i in range(0, nnet_input.size(1), self.chunk_size):
+                    if i + self.chunk_size > nnet_input.size(1):
+                        break
                     # 获取当前 chunk_size 个元素的块，并保持第一个维度不变
                     chunk = nnet_input[:, i:i + self.chunk_size]
                     on_test_start = time.time()
                     estim_src_tmp, estim_src_bn = self.model(chunk)
                     on_test_end = time.time()
                     cost_time = on_test_end - on_test_start
-                    print("Total inference chunk------------:", cost_time)
+                    print("Total inference chunk no win------------:", cost_time)
 
                     estim_src_0[0,i:i + self.chunk_size]= estim_src_tmp[0]
                     estim_src_1[0,i:i + self.chunk_size]= estim_src_tmp[1]
+                estim_src = [estim_src_0, estim_src_1]
+            elif out_win:
+                estim_src_0 = torch.zeros(1, nnet_input.size(1))
+                estim_src_1 = torch.zeros(1, nnet_input.size(1))
+                pre_estim_src_0 = torch.zeros(self.chunk_size)
+                pre_estim_src_1 = torch.zeros(self.chunk_size)
+                transition_length = 80
+                window = torch.from_numpy(np.hamming(2*transition_length)).float()
+                fade_in = window[:transition_length]
+                fade_out = window[transition_length:]
+
+                for i in range(0, nnet_input.size(1), self.chunk_size):
+                    if i + self.chunk_size > nnet_input.size(1):
+                        break
+                    # 获取当前 chunk_size 个元素的块，并保持第一个维度不变
+                    chunk = nnet_input[:, i:i + self.chunk_size]
+                    on_test_start = time.time()
+                    estim_src_tmp, estim_src_bn = self.model(chunk)
+                    on_test_end = time.time()
+                    cost_time = on_test_end - on_test_start
+                    print("Total inference chunk out win------------:", cost_time)
+
+                    signal0_fadeout = torch.cat((pre_estim_src_0[:-transition_length], fade_out * pre_estim_src_0[-transition_length:]), dim=0)
+                    signal0_fadein = torch.cat((fade_in * estim_src_tmp[0][0, :transition_length], estim_src_tmp[0][0, transition_length:]), dim=0)
+
+                    signal1_fadeout = torch.cat((pre_estim_src_1[:-transition_length], fade_out * pre_estim_src_1[-transition_length:]), dim=0)
+                    signal1_fadein = torch.cat((fade_in * estim_src_tmp[1][0, :transition_length], estim_src_tmp[1][0, transition_length:]), dim=0)
+
+                    smoothed_signal0 = torch.cat((signal0_fadeout, signal0_fadein), dim=0)
+                    smoothed_signal1 = torch.cat((signal1_fadeout, signal1_fadein), dim=0)
+
+                    pre_estim_src_0 = signal0_fadein
+                    pre_estim_src_1 = signal1_fadein
+                    if i-self.chunk_size >= 0:
+                        estim_src_0[0, i-self.chunk_size:i + self.chunk_size] = smoothed_signal0
+                        estim_src_1[0, i-self.chunk_size:i + self.chunk_size] = smoothed_signal1
+                    else:
+                        estim_src_0[0, i:i + self.chunk_size] = estim_src_tmp[0]
+                        estim_src_1[0, i:i + self.chunk_size] = estim_src_tmp[1]
+
+                estim_src = [estim_src_0, estim_src_1]
+            elif out_linear:
+                estim_src_0 = torch.zeros(1, nnet_input.size(1))
+                estim_src_1 = torch.zeros(1, nnet_input.size(1))
+                pre_estim_src_0 = torch.zeros(self.chunk_size)
+                pre_estim_src_1 = torch.zeros(self.chunk_size)
+                transition_length = 5
+
+                for i in range(0, nnet_input.size(1), self.chunk_size):
+                    if i + self.chunk_size > nnet_input.size(1):
+                        break
+                    # 获取当前 chunk_size 个元素的块，并保持第一个维度不变
+                    chunk = nnet_input[:, i:i + self.chunk_size]
+                    on_test_start = time.time()
+                    estim_src_tmp, estim_src_bn = self.model(chunk)
+                    on_test_end = time.time()
+                    cost_time = on_test_end - on_test_start
+                    print("Total inference chunk out linear------------:", cost_time)
+
+                    start_value0 = pre_estim_src_0[1-transition_length]
+                    end_value0 = estim_src_tmp[0][0, transition_length-1]
+                    interpolated_values = torch.from_numpy(np.linspace(start_value0, end_value0, 2*transition_length))
+                    pre_signal0 = torch.cat((pre_estim_src_0[:-transition_length], interpolated_values[:transition_length]), dim=0)
+                    signal0 = torch.cat((interpolated_values[transition_length:], estim_src_tmp[0][0, transition_length:]),dim=0)
+
+                    start_value1 = pre_estim_src_1[1-transition_length]
+                    end_value1 = estim_src_tmp[1][0, transition_length-1]
+                    interpolated_values = torch.from_numpy(np.linspace(start_value1, end_value1, 2*transition_length))
+                    pre_signal1= torch.cat((pre_estim_src_1[:-transition_length], interpolated_values[:transition_length]), dim=0)
+                    signal1 = torch.cat((interpolated_values[transition_length:], estim_src_tmp[1][0, transition_length:]),dim=0)
+
+                    smoothed_signal0 = torch.cat((pre_signal0, signal0), dim=0)
+                    smoothed_signal1 = torch.cat((pre_signal1, signal1), dim=0)
+
+                    pre_estim_src_0 = signal0
+                    pre_estim_src_1 = signal1
+                    if i - self.chunk_size >= 0:
+                        estim_src_0[0, i - self.chunk_size:i + self.chunk_size] = smoothed_signal0
+                        estim_src_1[0, i - self.chunk_size:i + self.chunk_size] = smoothed_signal1
+                    else:
+                        estim_src_0[0, i:i + self.chunk_size] = estim_src_tmp[0]
+                        estim_src_1[0, i:i + self.chunk_size] = estim_src_tmp[1]
+
+                estim_src = [estim_src_0, estim_src_1]
+            elif win_in_win_out:
+                window = torch.from_numpy(np.hamming(self.chunk_size))
+                window_sum = torch.zeros(nnet_input.size(1))
+                estim_src_0 = torch.zeros(1, nnet_input.size(1))
+                estim_src_1 = torch.zeros(1, nnet_input.size(1))
+
+                for i in range(0, nnet_input.size(1), self.hop_len):
+                    if i + self.chunk_size > nnet_input.size(1):
+                        break
+                    # 获取当前 chunk_size 个元素的块，并保持第一个维度不变
+                    chunk = nnet_input[:, i:i + self.chunk_size]
+                    chunk = (chunk * window).float()
+                    on_test_start = time.time()
+                    estim_src_tmp, estim_src_bn = self.model(chunk)
+                    #estim_src_tmp = [chunk, chunk]
+
+                    on_test_end = time.time()
+                    cost_time = on_test_end - on_test_start
+                    print("Total inference chunk win in win out------------:", cost_time)
+                    window_sum[i:i + self.chunk_size] += window
+
+                    estim_src_0[:, i:i + self.chunk_size] += estim_src_tmp[0]
+                    estim_src_1[:, i:i + self.chunk_size] += estim_src_tmp[1]
+                    estim_src_0[:, i:i + self.hop_len] /= window_sum[i:i + self.hop_len]
+                    estim_src_1[:, i:i + self.hop_len] /= window_sum[i:i + self.hop_len]
+
                 estim_src = [estim_src_0, estim_src_1]
             else:
                 window = np.hamming(frames.shape[0])
@@ -89,22 +204,27 @@ class Engine(object):
                     start = i * self.hop_len
                     chunk = frames_win_ten[:,i].unsqueeze(0).float()
                     on_test_start = time.time()
-                    estim_src_tmp, _ = self.model(chunk)
-                    #estim_src_tmp = [chunk, chunk]
+                    #estim_src_tmp, _ = self.model(chunk)
+                    estim_src_tmp = [chunk, chunk]
                     on_test_end = time.time()
                     cost_time = on_test_end - on_test_start
                     print("inference chunk", cost_time)
+                    window_sum[start:start + self.chunk_size] += window
+                    w = window_sum[start:start + self.chunk_size]
+
+                    print("wwww",window_sum[start:start + self.chunk_size])
 
                     estim_src_0[:, start:start + self.chunk_size] += estim_src_tmp[0]
                     estim_src_1[:, start:start + self.chunk_size] += estim_src_tmp[1]
-                    window_sum[start:start + self.chunk_size] += window
+                    estim_src_0[:, start:start + self.hop_len] /= window_sum[start:start + self.hop_len]
+                    estim_src_1[:, start:start + self.hop_len] /= window_sum[start:start + self.hop_len]
 
-                window_sum[window_sum == 0] = 1  # Avoid division by zero
+                #window_sum[window_sum == 0] = 1  # Avoid division by zero
                 estim_src_0 = estim_src_0.numpy().squeeze()  # 去掉多余的维度，变为形状 (N,)
                 estim_src_1 = estim_src_1.numpy().squeeze()  # 去掉多余的维度，变为形状 (N,)
 
-                estim_src_0 /= window_sum
-                estim_src_1 /= window_sum
+                #estim_src_0 /= window_sum
+                #estim_src_1 /= window_sum
                 estim_src_0 = torch.tensor(estim_src_0).unsqueeze(0)
                 estim_src_1 = torch.tensor(estim_src_1).unsqueeze(0)
 
@@ -115,13 +235,31 @@ class Engine(object):
                 if wav_dir and not os.path.exists(wav_dir): os.makedirs(wav_dir)
                 mixture = torch.squeeze(mixture).cpu().data.numpy()
                 sf.write(os.path.join(wav_dir, mxiture_file + '.wav'),
-                         0.5 * mixture / max(abs(mixture)), 8000)
+                         mixture, 8000)
+                src0 = torch.squeeze(estim_src[0]).cpu().data.numpy()
+                src1 = torch.squeeze(estim_src[1]).cpu().data.numpy()
+                max_src = max(max(abs(src0)), max(abs(src1)))
+
                 for i in range(self.config['model']['num_spks']):
                     src = torch.squeeze(estim_src[i]).cpu().data.numpy()
+                    print(max(abs(src)))
+
                     if self.non_chunk:
                         sf.write(os.path.join(wav_dir, mxiture_file + f'_T_Infer_Nonchunk_output_{i}.wav'),
-                                 0.5 * src / max(abs(src)), 8000)
+                                 src / max(max_src, 3000.0), 8000)
+                    elif no_win:
+                        sf.write(os.path.join(wav_dir, mxiture_file + f'_T_Infer_Chunk_output_{i}.wav'),
+                                 src / max(max_src, 3000.0), 8000)
+                    elif out_win:
+                        sf.write(os.path.join(wav_dir, mxiture_file + f'_T_Infer_Chunk_outwin_{i}.wav'),
+                                 src / max(max_src, 3000.0), 8000)
+                    elif out_linear:
+                        sf.write(os.path.join(wav_dir, mxiture_file + f'_T_Infer_Chunk_outlinear_{i}.wav'),
+                                 src / max(max_src, 3000.0), 8000)
+                    elif win_in_win_out:
+                        sf.write(os.path.join(wav_dir, mxiture_file + f'_T_Infer_Chunk_winInwinOut_{i}.wav'),
+                                 src / max(max_src, 3000.0), 8000)
                     else:
                         sf.write(os.path.join(wav_dir, mxiture_file + f'_T_Infer_Chunk_output_{i}.wav'),
-                                 0.5 * src / max(abs(src)), 8000)
+                                 src, 8000)
         return
